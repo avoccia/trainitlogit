0bf3ee25af6cd27ad2652e17b2087caccd2c4233
Alyssa <avoccia@hotmail.com>
2017-11-17 10:22:13 -0500

    Setup formatting for time and createdAt

--- src/components/WorkoutListItem.js
+++ src/components/WorkoutListItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const WorkoutListItem = ({id, description, time, createdAt}) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{time} - {createdAt}</p>
+    <p>
+      {numeral(time * 60).format('00:00:00')} 
+      - 
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
